acad5e9e5c400e0487bc0ecdc0f161c921f91f8d
soyoon.seol <soyoon.seol@emotion.co.kr>
2024-01-24 10:28:59 +0900

    #1.2 difference between useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Button from "./Button";
-
+// import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("Call the API...");
+  }, []);
   return (
     <div className="App">
-      <h1>여기가 바로 그저 CGV</h1>
-      <Button text={"입장하거라"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here"/>
+      <h1>{ counter }</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
