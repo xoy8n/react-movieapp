95c11b81930df4abe631f3f5a4ee178da84352e3
soyoon.seol <soyoon.seol@emotion.co.kr>
2024-06-18 07:56:06 +0900

    #3.1 todolist 만들기

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-// import Button from "./Button";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created!!!");
-    //cleanup fucntion!!!!!!
-    //컴포넌트가 destroy될 때 뭔가 할 수 있도록 해줌
-    return () => console.log("destroyed ㅠㅠ");
-  }, []);
-  return <h1>hello</h1>;
-}
-
-//위의 Hello functional component와 같은 원리!
-function Hi() {
-  function hiFn() {
-    console.log("created!!");
-    return byFn;
-  }
-  function byFn() {
-    console.log("destroyed ㅜㅜ");
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClickBtn = () => setShowing((a) => !a);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChangeToDo = (e) => setToDo(e.target.value);
+  const onStopSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
 
   return (
     <div className="App">
-      {showing ? <Hi /> : null}
-      <button onClick={onClickBtn}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onStopSubmit}>
+        <input
+          onChange={onChangeToDo}
+          value={toDo}
+          type="text"
+          placeholder="할일을 입려해라!"
+        />
+        <button>할일 추가하기</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, a) => (
+          <li key={a}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
